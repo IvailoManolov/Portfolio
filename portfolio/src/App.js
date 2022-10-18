7fa0dcacb6510c7631b2aabd82d8620e3d56e56e
IvailoManolov <ivomoreras@abv.bg>
2022-10-19 00:03:54 +0300

    added navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 
 import {About,Footer,Header,Skills,Work} from "./containers"
+import {Navbar} from "./components"
 
 function App() {
   return (
     <div className='app'>
 
+      <Navbar/>
       <Header />
       <About />
       <Work />
       <Skills />
       <Footer />
-
+      
     </div>
   );
 }

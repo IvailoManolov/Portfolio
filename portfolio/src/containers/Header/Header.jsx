8888719b63eb64c85deb57b8aff546bb5ce52847
IvailoManolov <ivomoreras@abv.bg>
2022-10-24 23:37:43 +0300

    added wrapper for social media

--- portfolio/src/containers/Header/Header.jsx
+++ portfolio/src/containers/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {motion} from 'framer-motion'
 
 import {images} from "../../constants"
+import {AppWrap} from '../../wrapper'
 
 import "./Header.scss"
 const scaleVariants = {
@@ -15,9 +16,8 @@ const scaleVariants = {
   }
 }
 const Header = () => {
-
   return (
-    <div id="home" className="app__header app__flex">
+    <div className="app__header app__flex">
       <motion.div
       whileInView={{x : [-100,0] , opacity : [0,1]}}
       transition = {{duration : 1.5}}
@@ -72,4 +72,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default AppWrap(Header,'home')
